d3ff62ae633f485e84061813b87250c3d6460754
nati-tewolde <natisnewemail@gmail.com>
2025-12-04 15:26:46 -0500

    Initial commit

---
+++ src/main/java/com/pluralsight/NorthwindTradersSpringBootPt2/NorthwindTradersSpringBootPt2Application.java
@@ -0,0 +1,13 @@
+package com.pluralsight.NorthwindTradersSpringBootPt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthwindTradersSpringBootPt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthwindTradersSpringBootPt2Application.class, args);
+	}
+
+}
